d2b1c9750bdb6ac252b7dd8260ff94fa96d20375
StevenTai <stevenmetai48@gmail.com>
2014-12-18 15:42:41 +0800

    add toast

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -18,7 +19,7 @@ public class MainActivity extends ActionBarActivity {
         {
             public void onClick(View v)
             {
-                
+                Toast.makeText(getApplicationContext(), "Hello man", Toast.LENGTH_LONG).show();
             }
 
         });
